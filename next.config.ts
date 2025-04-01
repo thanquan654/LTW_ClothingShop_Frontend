9f119803231a7d7724b51bdd52f5f7f3252d5b13
Quan Than <quantv654@gmail.com>
2025-04-01 20:39:42 +0700

    feat: implement 404 Page

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+	i18n: {
+		locales: ['vi', 'en'],
+		defaultLocale: 'vi',
+	},
+	reactStrictMode: true,
+}
 
-export default nextConfig;
+export default nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextComponentType, NextPageContext } from 'next'
+import NProgress from 'nprogress'
+import Router, { useRouter } from 'next/router'
+import { NextIntlClientProvider } from 'next-intl'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import '../styles/globals.css'
+import 'animate.css'
+import 'nprogress/nprogress.css'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+type AppCustomProps = {
+	Component: NextComponentType<NextPageContext, any, object>
+	pageProps: any
+}
+
+export default function App({ Component, pageProps }: AppCustomProps) {
+	const router = useRouter()
+
+	return (
+		<NextIntlClientProvider
+			locale={router.locale}
+			timeZone="Asia/Ho_Chi_Minh"
+			messages={pageProps?.messages}
+		>
+			<Component {...pageProps} />
+		</NextIntlClientProvider>
+	)
 }
