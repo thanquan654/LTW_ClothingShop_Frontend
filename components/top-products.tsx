04daa29662c35d3b8ccf6136031842367014718a
Quan Than <quantv654@gmail.com>
2025-04-25 09:00:57 +0700

    chore: update

--- components/top-products.tsx
+++ components/top-products.tsx
@@ -13,7 +13,7 @@ export function TopProducts({ products }: { products: Product[] }) {
 						<th className="py-2 px-3 font-semibold">
 							Tên sản phẩm
 						</th>
-						<th className="py-2 px-3 font-semibold">Mã biến thể</th>
+						<th className="py-2 px-3 font-semibold">Biến thể</th>
 						<th className="py-2 px-3 font-semibold">
 							Số lượng bán
 						</th>
@@ -26,7 +26,9 @@ export function TopProducts({ products }: { products: Product[] }) {
 							className="border-b last:border-b-0 hover:bg-gray-50 transition"
 						>
 							<td className="py-2 px-3">{product.tenSanPham}</td>
-							<td className="py-2 px-3">{product.maBienThe}</td>
+							<td className="py-2 px-3">
+								{product.tenMau} - {product.tenKichCo}
+							</td>
 							<td className="py-2 px-3 font-bold text-green-700">
 								{product.soLuongBan}
 							</td>
